05df4c4604eb6ef4acf303862d8108df6fc67ca7
dixonaws <dixonaws@amazon.com>
2017-03-28 14:03:40 -0400

    add gzip functionalilt to Sync script

--- SyncData.sh
+++ SyncData.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-aws --profile dixonaws@amazon.com s3 sync data/csv/ s3://fleetbriefing-data/data/ --acl "public-read" --delete
+# generate gzip files
+for i in $(ls -lah data/csv |awk '{print $9}'); do gzip --fast --keep data/csv/$i; done
+for i in $(ls -lah data/csv |grep gz |awk '{print $9}'); do mv data/csv/$i data/gzip/; done
+
+aws --profile dixonaws@amazon.com s3 sync data/csv/ s3://fleetbriefing-data/data/csv --acl "public-read" --delete
